656a06368e74213a40a22c6f34ed0953c7806fc6
Natalie-Cardot <nataliecardot@gmail.com>
2020-08-13 09:52:40 -0700

    Use Oak framework with Deno

--- app.ts
+++ app.ts
@@ -1,9 +1,9 @@
-import { serve } from "https://deno.land/std/http/server.ts";
+import { Application } from "https://deno.land/x/oak/mod.ts";
 
-// Network requests blocked by default, to open up a server, which is related to network, need extra permission flag: `deno run --allow-net app.ts`
-const server = serve({ port: 3000 });
+const app = new Application();
 
-// server is an async iterable, like an array full of promises. Server creates new promises which we can await, and a new promise that resolves is generated for every incoming request
-for await (const req of server) {
-  req.respond({ body: "Hello world\n" });
-}
+app.use((ctx) => {
+  ctx.response.body = "Hello World!";
+});
+
+await app.listen({ port: 8000 });
